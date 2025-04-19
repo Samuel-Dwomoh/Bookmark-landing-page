a2b5bf7f5c47b9a86fbf7aa76bca85466b0d1e96
Samuel Dwomoh <sdwomoh418@gmail.com>
2025-04-19 21:20:42 +0000

    toggle section completed

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 //Loading html elements
 const pbrowser = document.getElementById("browser");
 const below = document.getElementById("first");
-
+const maindiv = document.getElementById("maindiv");
 // Create paragraph once and append it
 const ptag = document.createElement("p");
-ptag.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit...";
-ptag.classList.add("font-medium", "text-black", "mt-3", "w-full", "hidden");
-pbrowser.appendChild(ptag);
+ptag.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsim sun de tak likye shjoos woru roiya otoe oao sdsd edssaeg wyouew w orw rw rouworuwuoruworw wouw owuo wo w oowpw woere wouryw owryuoey wowsyov sov osyoustsyts ssi";
+ptag.classList.add("font-medium", "text-black", "mt-3", "w-full", "hidden", "mb-3");
+maindiv.appendChild(ptag);
 
 // Toggle visibility on click
 below.addEventListener("click", () => {
